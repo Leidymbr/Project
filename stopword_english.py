deca6b9d357fd32af31459c3b3889297bdf536c2
Leidymbr <leidyb0824@gmail.com>
2025-07-31 19:47:32 -0500

    ejercicio practico con nltk

--- stopword_english.py
+++ stopword_english.py
@@ -1,14 +1,1 @@
-import nltk
 
-# desde nltk descargar stopwords
-from nltk.corpus import stopwords
-nltk.download('stopwords')
-lista_stopwords = stopwords.words('english')
-# imprimir las stopwords
-print(lista_stopwords)
-
-#importar la libreria nltk
-import nltk
-from nltk.corpus import stopwords
-nltk.download('stopwords')
-nltk.data.path.append('C:/Users/Leidy/Downloads/nltk_data')
